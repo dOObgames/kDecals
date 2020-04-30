af013bfa97e770494bdb3049f2736e35b2f2210a
Alex Bagnolini <alex.bagnolini@doobgames.com>
2020-04-30 18:44:54 +0200

    Fixed layermask selection

--- Editor/DecalDataEditor.cs
+++ Editor/DecalDataEditor.cs
@@ -175,8 +175,9 @@ namespace kTools.Decals.Editor
 
             // Layer Mask
             EditorGUI.BeginChangeCheck();
-            LayerMask tempMask = EditorGUILayout.MaskField(Styles.LayerMask, (LayerMask)m_LayerMaskProp.intValue, InternalEditorUtility.layers);
-            if(EditorGUI.EndChangeCheck())
+            LayerMask tempMask = EditorGUILayout.MaskField(Styles.LayerMask, InternalEditorUtility.LayerMaskToConcatenatedLayersMask((LayerMask)m_LayerMaskProp.intValue), InternalEditorUtility.layers);
+            tempMask = InternalEditorUtility.ConcatenatedLayersMaskToLayerMask(tempMask);
+            if (EditorGUI.EndChangeCheck())
             {
                 m_LayerMaskProp.intValue = (int)tempMask;
             }
